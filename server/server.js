36af9d6480f8386dc442be89eea11c4856b947ae
Marcelo Segura <marcelo@localhost.localdomain>
2020-06-06 03:43:17 -0300

    comenzando el get

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -12,41 +15,23 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL!!!');
+app.use( require('./routes/usuario'));
+
+
+// conección a DB
+
+mongoose.connect('mongodb://localhost/cafe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
+},  (err, resp) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
